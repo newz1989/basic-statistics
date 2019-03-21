370d65f59673085bf7008e1793fc53c44ef4f6c8
dzlobenets <d.zlobenets@trafficdna.com>
2019-03-22 00:40:39 +0200

    add avg

--- stat-library/src/main/java/ua/com/dzlobenets/stat/lib/MeasureOfCentralTendency.java
+++ stat-library/src/main/java/ua/com/dzlobenets/stat/lib/MeasureOfCentralTendency.java
@@ -5,10 +5,16 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+/**
+ * This class describe base methods for Measure of central tendency
+ * {mode, average, median}
+ */
 public class MeasureOfCentralTendency {
-
+    /**
+     * return value which occur maximum times
+     */
     @Nonnull
-    public static <T> T mode(@Nonnull Collection<? extends T> data) {
+    public static <T extends Number> T mode(@Nonnull Collection<T> data) {
         final Map<T, Long> collect = data.stream().collect(
                 Collectors.groupingBy(
                         s -> s,
@@ -19,6 +25,12 @@ public class MeasureOfCentralTendency {
                 .max(Map.Entry.comparingByValue())
                 .map(Map.Entry::getKey)
                 .get();
+    }
+
+
+    public static <T extends Number> Double avg(@Nonnull Collection<T> data) {
+        return data.stream()
+                .collect(Collectors.averagingDouble(Number::doubleValue));
     }
 
 }
